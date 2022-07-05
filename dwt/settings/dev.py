3e8322dc7d85994b490fddb514e213f35faee9ad
Atinos31 <atinos31.dev@gmail.com>
2022-07-05 19:24:25 +0000

    initial commit

---
+++ dwt/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--7z-@*a=y4cf+=1vpbjwk60%+7=n6#^+@t+oz)kip1m6$3p1%f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
